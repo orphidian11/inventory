bacf14b5c13831e4d0b02199562f57a2937c7a34
orphidian11 <orphidian11@gmail.com>
2020-02-08 21:44:03 +0800

    Initial Commit

---
+++ src/main/java/net/orphidian11/projects/cars/inventory/InventoryApplication.java
@@ -0,0 +1,13 @@
+package net.orphidian11.projects.cars.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryApplication.class, args);
+	}
+
+}
